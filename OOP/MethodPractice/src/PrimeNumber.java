4a5d3ddaae462b7ded876122bc62c01ea06f93e8
Seemanto Barman <seemanto@hotmail.com>
2021-08-28 01:30:46 +0600

    prime number

---
+++ OOP/MethodPractice/src/PrimeNumber.java
@@ -0,0 +1,14 @@
+public class PrimeNumber {
+    static boolean checkPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPrime(19));
+    }
+}
